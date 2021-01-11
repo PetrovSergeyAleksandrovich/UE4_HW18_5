d0fc48400bbeaaa4f479e419d62515da96f50efd
PetrovSergeyAleksandrovich <ps@dsys.ru>
2021-01-11 18:36:39 +0300

    hw done

--- UE4_HW18_5.cpp
+++ UE4_HW18_5.cpp
@@ -1,6 +1,52 @@
 #include <iostream>
 
+class Stack
+{
+
+private:
+    int x = 5;
+    int mas[5] = { 1,2,3,4,5 };
+
+public:
+    void push()
+    {
+        int a;
+        std::cout << "\nType integer:";
+        std::cin >> a;
+        mas[x++] = a;
+    }
+    void printStack()
+    {
+        for (int i = 0; i < x; i++)
+        {
+            std::cout << mas[i] << " ";
+        }
+        std::cout << std::endl;
+    }
+    void pop()
+    {
+        std::cout << "\nLast integer "<< mas[x-1] << " was removed from stack"<< "\n";
+        mas[x--];
+    }
+};
+
+
 int main()
 {
-    std::cout << "/fuck world\n";
+
+    Stack* data = new Stack;
+
+    //always growing stack
+    for (;;)
+    {
+        data->push(); //add integer
+        data->printStack();
+        data->push(); //add integer
+        data->printStack();
+
+        data->pop(); //remove last integer
+        data->printStack();
+    }
+
+    return 0;
 }
